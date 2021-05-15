d76c45b50714efe8b04c78b1755d9f9e3203d752
dk.raw <hello@dkatsios.tk>
2021-05-15 13:41:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindLargestNumber();
+        }
+        private static void FindLargestNumber()
+        {
+            int[] p = new int[] { 5, 8, 9, 2, 3, 7 };
+            int max = p[0];
+            for (int i = 1; i < p.Length; i++)
+            {
+                if (p[i] > max)
+                {
+                    max = p[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
